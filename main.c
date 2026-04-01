dc9ef3b6461d253b392daf629e2d3c163a972fc1
JunKai Cai <2184377169@qq.com>
2026-04-01 11:30:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "Delay.h"
 #include "lcd1602.h"
 #include "cat_font.h"
-
+//将自定义的猫咪字符写入lcd的cgram，便于后续显示
 void load_cat_chars(void)
 {
     lcd_write_cgram(0, cat_ear_l);
@@ -14,12 +14,12 @@ void load_cat_chars(void)
     lcd_write_cgram(6, cat_eye_close_l);
     lcd_write_cgram(7, cat_eye_close_r);
 }
-
+//在lcd上绘制猫咪图案，blink = 1 时闭眼 ， blink = 0 睁开眼睛
 void draw_cat(unsigned char blink)
 {
     unsigned char eye_l = blink ? 6 : 3;
     unsigned char eye_r = blink ? 7 : 5;
-
+// 根据blink状态选择使用睁眼还是闭眼的自定义字符
     lcd_set_cursor(0, 7);
     lcd_write_data(0);
     lcd_write_data(1);
@@ -30,7 +30,7 @@ void draw_cat(unsigned char blink)
     lcd_write_data(4);
     lcd_write_data(eye_r);
 }
-
+// 主函数：初始化lcd，循环显示猫咪眨眼效果
 void main(void)
 {
     unsigned char state = 0;
